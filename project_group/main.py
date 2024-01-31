0f5e6a7fc309a76aef351527f9337e7dc6313848
Erwin-Yeo <157445387+Erwin-Yeo@users.noreply.github.com>
2024-02-01 00:43:30 +0800

    Add 2

--- project_group/main.py
+++ project_group/main.py
@@ -3,3 +3,17 @@ import cash_on_hand, profit_loss, overheads
 
 file_path = Path(r"C:\Users\jessl\OneDrive\Microsoft Teams Chat Files\visual studio code\summary_report.txt")
 file_path.touch()
+
+with file_path.open(mode="w", encoding="UTF-8") as file:
+    overheads_output = overheads.overheads_function()
+    cash_output = cash_on_hand.cash_function()
+    profit_output = profit_loss.profit_function()
+
+    # Check and convert None to an empty string
+    overheads_output = overheads_output or ""
+    cash_output = cash_output or ""
+    profit_output = profit_output or ""
+
+    file.write(overheads_output)
+    file.write(cash_output)
+    file.write(profit_output)
