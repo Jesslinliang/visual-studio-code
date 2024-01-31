51f71aaf3641a7843de573227fd84be5b1e654ac
Erwin-Yeo <157445387+Erwin-Yeo@users.noreply.github.com>
2024-02-01 00:42:15 +0800

    Add

---
+++ project_group/main.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+import cash_on_hand, profit_loss, overheads
+
+file_path = Path(r"C:\Users\jessl\OneDrive\Microsoft Teams Chat Files\visual studio code\summary_report.txt")
+file_path.touch()
